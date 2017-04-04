4246e5c38136796b4c7da4e0852a92afd8dd111a
AleksandrVinokurov <avvin84@gmail.com>
2017-04-04 16:55:46 +0500

    Works

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var fortune = require('./lib/fortune.js');
 
+
 var express = require('express');
 var app = express();
 var handlebars = require('express-handlebars').create({ defaultLayout: 'main' });
@@ -7,17 +8,36 @@ var handlebars = require('express-handlebars').create({ defaultLayout: 'main' })
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
+app.use(function(req, res, next){
+    res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+    next();
+});
+
 app.use(express.static(__dirname + '/public'));
 
 app.set('port', process.env.PORT || 3000);
+
+
 
 app.get('/', function(req, res) {
     res.render('home');
 });
 
 app.get('/about', function(req, res) {
-    res.render('about', { fortune: fortune.getFortune() } );
+    res.render('about', {
+        fortune: fortune.getFortune(),
+        pageTestScript: '/qa/tests-about.js'
+    } );
 });
+
+app.get('/tours/hood-river', function(req, res){
+    res.render('tours/hood-river');
+});
+
+app.get('/tours/request-group-rate', function(req, res){
+    res.render('tours/request-group-rate');
+});
+
 
 
 app.use(function(req, res, next){
@@ -31,6 +51,7 @@ app.use(function(err, req, res, next){
     res.status(500);
     res.render('500');
 });
+
 
 app.listen(app.get('port'), function(){
     console.log( 'Express запущен на http://localhost:' +
