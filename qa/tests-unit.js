39bc1b83d66d7efdb91e3d53ed2c1ee0778b6535
AleksandrVinokurov <avvin84@gmail.com>
2017-04-10 09:41:58 +0500

    Fix

--- qa/tests-unit.js
+++ qa/tests-unit.js
@@ -1,7 +1,10 @@
 var fortune = require('../lib/fortune.js');
 var expect = require('chai').expect;
-suite('Тесты печений-предсказаний', function(){
-    test('getFortune() должна возвращать предсказание', function(){
+
+suite('Fortune cookie tests', function(){
+
+    test('getFortune() should return a fortune', function(){
         expect(typeof fortune.getFortune() === 'string');
     });
+
 });
